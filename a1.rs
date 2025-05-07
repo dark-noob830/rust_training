e5e6389c2cff9bb000356fbc9ae7df5b2d241d96
dark-noob830 <mahdiamr83@gmail.com>
2025-05-07 20:11:09 +0330

    exercise1

---
+++ a1.rs
@@ -0,0 +1,20 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn my_first() {
+    println!("mahdi")
+}
+fn last_name() {
+    println!("amrollahi")
+}
+fn main() {
+    my_first();
+    last_name();
+}
